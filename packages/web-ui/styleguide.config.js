718b0f7788fa332fd54ac917b9bbbc1d3b35e043
krazijames <krazijames@ridi.com>
2018-11-22 14:12:21 +0900

    Change styleguide's component path line

--- packages/web-ui/styleguide.config.js
+++ packages/web-ui/styleguide.config.js
@@ -1,8 +1,14 @@
+const path = require('path');
 const { parse: propsParser } = require('react-docgen-typescript');
 const webpackConfig = require('./webpack.config');
+const pkg = require('./package.json');
 
 module.exports = {
   components: 'src/*/index.{ts,tsx}',
+  getComponentPathLine: (componentPath) => {
+    const componentName = path.basename(path.dirname(componentPath));
+    return `import { ${componentName} } from '${pkg.name}';`;
+  },
   propsParser,
   usageMode: 'expand',
   exampleMode: 'expand',
