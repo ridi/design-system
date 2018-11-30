d2f0147e0b67775c551b43fe746aeaf1409dfd92
krazijames <krazijames@ridi.com>
2018-11-30 17:17:31 +0900

    Apply SVGR options

--- packages/web-ui/webpack.config.js
+++ packages/web-ui/webpack.config.js
@@ -34,7 +34,15 @@ module.exports = {
       },
       {
         test: /\.svg$/,
-        use: ['@svgr/webpack'],
+        use: [
+          {
+            loader: '@svgr/webpack',
+            options: {
+              prettier: false,
+              svgo: false,
+            },
+          },
+        ],
       },
     ],
   },
