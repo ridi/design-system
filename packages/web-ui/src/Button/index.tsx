1953f21db30fb00ad824312626421f0b7e8bf560
krazijames <krazijames@ridi.com>
2018-12-18 12:17:13 +0900

    Add description of props

--- packages/web-ui/src/Button/index.tsx
+++ packages/web-ui/src/Button/index.tsx
@@ -3,7 +3,14 @@ import { Base, BaseProps } from '../Base';
 import style from './style';
 
 export interface ButtonProps extends BaseProps {
+  /**
+   * Main color of `Button`.
+   */
   color?: 'gray' | 'blue' | 'brown';
+
+  /**
+   * If `true`, `Button` will be rendered with colored border and white background.
+   */
   outline?: boolean;
 }
 
