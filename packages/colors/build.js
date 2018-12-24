69cb03fb530634c4956ced9ea039587ba786a047
krazijames <krazijames@ridi.com>
2018-12-24 17:04:09 +0900

    Generate index.d.ts

--- packages/colors/build.js
+++ packages/colors/build.js
@@ -3,8 +3,27 @@ const fs = require('fs');
 const path = require('path');
 const chalk = require('chalk');
 const Color = require('color');
+const pkg = require('./package.json');
 
 const sourceColors = require('./hwb.json');
+
+const generateModuleDefinition = () => {
+  const fileName = 'index.d.ts';
+
+  const statements = _.map(sourceColors, (color, name) => (
+    `  const ${name}: string;`
+  )).join('\n');
+
+  const data = `\
+declare module '${pkg.name}' {
+${statements}
+}
+`;
+
+  fs.writeFileSync(path.join(__dirname, fileName), data);
+
+  console.log(`- Generated ${fileName}`);
+};
 
 const convertColors = (colors, converter, fileName) => {
   const convertedColors = _.mapValues(colors, converter);
@@ -14,6 +33,7 @@ const convertColors = (colors, converter, fileName) => {
   console.log(`- Generated ${fileName}`);
 };
 
+generateModuleDefinition();
 convertColors(sourceColors, color => Color(color).hex(), 'hex.json');
 convertColors(sourceColors, color => Color(color).rgb().string(), 'rgb.json');
 convertColors(sourceColors, color => Color(color).hsl().string(), 'hsl.json');
