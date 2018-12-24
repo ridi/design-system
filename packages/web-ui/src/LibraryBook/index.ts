0a88103c01704e17bc8515b12fa592ad25e8d61f
krazijames <krazijames@ridi.com>
2018-12-24 17:58:46 +0900

    Fix Styleguidist settings

--- packages/web-ui/src/LibraryBook/index.ts
+++ packages/web-ui/src/LibraryBook/index.ts
@@ -1,2 +1,3 @@
 import * as LibraryBook from './LibraryBook';
+
 export { LibraryBook };
--- packages/web-ui/styleguide.config.js
+++ packages/web-ui/styleguide.config.js
@@ -1,15 +1,28 @@
 const path = require('path');
-const { parse: propsParser } = require('react-docgen-typescript').withDefaultConfig();
+const { parse: propsParser } = require('react-docgen-typescript');
 const webpackConfig = require('./webpack.config');
 const pkg = require('./package.json');
 
 module.exports = {
-  components: 'src/*/index.{ts,tsx}',
   getComponentPathLine: (componentPath) => {
     const componentName = path.basename(path.dirname(componentPath));
     return `import { ${componentName} } from '${pkg.name}';`;
   },
-  // propsParser,
+  sections: [
+    {
+      name: 'Components',
+      components: 'src/*/index.{ts,tsx}',
+      ignore: 'src/LibraryBook/index.{ts,tsx}',
+    },
+    {
+      name: 'LibraryBook',
+      content: 'src/LibraryBook/README.md',
+    },
+  ],
+  context: {
+    LibraryBook: path.join(__dirname, 'src/LibraryBook/LibraryBook'),
+  },
+  propsParser,
   usageMode: 'expand',
   editorConfig: {
     theme: 'dracula',
