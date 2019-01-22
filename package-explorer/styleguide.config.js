ae1bac3942fef0bceec8ac240a3b85be314d65d1
krazijames <krazijames@ridi.com>
2019-01-22 19:15:44 +0900

    Add sections

--- package-explorer/styleguide.config.js
+++ package-explorer/styleguide.config.js
@@ -4,20 +4,36 @@ const webUiWebpackConfig = require('../packages/web-ui/webpack.config.web');
 const webUiPackage = require('../packages/web-ui/package.json');
 
 module.exports = {
-  components: '../packages/web-ui/src/*/index.{ts,tsx}',
-  getComponentPathLine: (componentPath) => {
+  pagePerSection: true,
+  sections: [
+    {
+      name: "colors",
+      usageMode: "hide",
+      exampleMode: "hide"
+    },
+    {
+      name: "web-icons",
+      usageMode: "hide",
+      exampleMode: "hide"
+    },
+    {
+      name: 'web-ui',
+      components: "../packages/web-ui/src/*/index.{ts,tsx}",
+      usageMode: "expand",
+    }
+  ],
+  getComponentPathLine: componentPath => {
     const componentName = path.basename(path.dirname(componentPath));
     return `import { ${componentName} } from '${webUiPackage.name}';`;
   },
   propsParser,
-  usageMode: 'expand',
   editorConfig: {
-    theme: 'dracula',
-    smartIndent: true,
+    theme: "dracula",
+    smartIndent: true
   },
   webpackConfig: {
     ...webUiWebpackConfig,
-    mode: 'development',
-    devtool: 'eval-source-map',
-  },
+    mode: "development",
+    devtool: "eval-source-map"
+  }
 };
