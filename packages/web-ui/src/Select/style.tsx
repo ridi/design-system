9add53e8930e94b1328b1bbfec12eaca6ddba015
krazijames <krazijames@gmail.com>
2019-01-25 12:33:09 +0900

    Use camelCase for colors and icons (#34)
    * Rename colors to camelCase
    
    * Rename icons to camelCase
    
    * Update CHANGELOG
    
    * Fix errors
    
    * Use snake_case for SVG files

--- packages/web-ui/src/Select/style.tsx
+++ packages/web-ui/src/Select/style.tsx
@@ -1,5 +1,5 @@
 import colors from '@ridi/colors';
-import { arrow_1_down } from '@ridi/web-icons';
+import { arrow1Down } from '@ridi/web-icons';
 import { merge } from 'lodash';
 import * as React from 'react';
 import { formElementDisabled, formElementHover, resetAppearance, resetFont, resetLayout } from '../styles';
@@ -7,7 +7,7 @@ import { Svg, SvgProps } from '../Svg';
 import { SelectProps } from './index';
 
 const ArrowIcon = (props: SvgProps) => (
-  <Svg render={arrow_1_down} {...props} />
+  <Svg render={arrow1Down} {...props} />
 );
 
 export default ({ outline }: SelectProps) => merge({},
@@ -25,7 +25,7 @@ export default ({ outline }: SelectProps) => merge({},
     paddingRight: '1.7em',
     outline: 'none',
     background: 'white',
-    color: colors.slategray_70,
+    color: colors.slateGray70,
     fontSize: '13px',
     fontWeight: 700,
     touchAction: 'manipulation',
@@ -34,7 +34,7 @@ export default ({ outline }: SelectProps) => merge({},
     backgroundPosition: 'right 7px center',
     backgroundRepeat: 'no-repeat',
     backgroundSize: '10px 6px',
-    backgroundImage: `url(${Svg.renderToDataUri(<ArrowIcon fill={colors.slategray_50} />)})`,
+    backgroundImage: `url(${Svg.renderToDataUri(<ArrowIcon fill={colors.slateGray50} />)})`,
 
     '&::-ms-expand': {
       display: 'none',
@@ -43,13 +43,13 @@ export default ({ outline }: SelectProps) => merge({},
     '&:not(:disabled)': {
       '&:hover, &:active': {
         ...formElementHover,
-        color: colors.slategray_90,
+        color: colors.slateGray90,
       },
     },
 
     '&:disabled': {
-      color: colors.slategray_40,
-      backgroundImage: `url(${Svg.renderToDataUri(<ArrowIcon fill={colors.slategray_20} />)})`,
+      color: colors.slateGray40,
+      backgroundImage: `url(${Svg.renderToDataUri(<ArrowIcon fill={colors.slateGray20} />)})`,
       cursor: 'not-allowed',
     },
   },
@@ -57,7 +57,7 @@ export default ({ outline }: SelectProps) => merge({},
   outline && {
     padding: '.6em',
     paddingRight: '1.7em',
-    border: `1px solid ${colors.slategray_20}`,
+    border: `1px solid ${colors.slateGray20}`,
     borderRadius: '3px',
 
     '&:disabled': formElementDisabled,
