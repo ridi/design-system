9a5ac225759ebde05bcab3fd726204035ae98bce
Heejae Jang <j132134@gmail.com>
2020-01-30 12:22:54 +0900

    Remove package-explorer, packages/web-ui

--- packages/web-ui/svg.d.ts
+++
@@ -1,7 +0,0 @@
-declare module '*.svg' {
-  /**
-   * Exported by `@svgr/webpack`.
-   */
-  const ReactComponent: React.ComponentType<React.SVGProps<SVGSVGElement>>;
-  export default ReactComponent;
-}
